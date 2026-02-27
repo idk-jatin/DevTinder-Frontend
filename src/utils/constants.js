2b666a90e88fa06b87d058a3aa4bc9f55cba8832
idk-jatin <jatinkrpandit40@gmail.com>
2026-02-27 17:18:26 +0530

    fix: url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,1 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL || "https://devtinder-8kcw.onrender.com";
+export const BASE_URL = import.meta.env.VITE_BASE_URL === "undefined" || !import.meta.env.VITE_BASE_URL ? "https://devtinder-8kcw.onrender.com" : import.meta.env.VITE_BASE_URL;
